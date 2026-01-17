3fdfdfcd9c5210582acae7f41fd4717a2f250714
memishra9 <38108646+memishra9@users.noreply.github.com>
2026-01-17 13:49:40 +0000

    click decorator to use cli for passing parameters

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 
 from tqdm.auto import tqdm
 import sys
+import click
 
 
 dtype = {
@@ -29,23 +30,26 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
-def run():
-    year=2021
-    month='01'
-    pg_user='root'
-    pg_password='root'
-    pg_host='localhost'
-    pg_db='ny_taxi'
-    pg_port='5432'
-    chunksize=10000
-    target_table='yellow_taxi_data'
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+
     prefix="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
-    url= f"{prefix}yellow_tripdata_{year}-{month}.csv.gz"
-    #'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz'
+
+    url= f"{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz"
+
     
-    print(f'URL is: {url}')
-    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/ny_taxi')
+
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
     
     df_iter=pd.read_csv(
         url, 
